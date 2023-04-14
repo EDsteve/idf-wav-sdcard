3776d95ddee0cd0d75e58a7c2deca25b8154fecc
fabi <109753539+LIFsCode@users.noreply.github.com>
2023-04-14 12:27:30 +0200

    Adjust config.h for ELOC 3.0 HW

--- src/config.h
+++ src/config.h
@@ -5,7 +5,7 @@
 // #define USE_SPIFFS 1
 
 // sample rate for the system
-#define SAMPLE_RATE 44000
+#define SAMPLE_RATE 20000
 
 // are you using an I2S microphone - comment this out if you want to use an analog mic and ADC input
 #define USE_I2S_MIC_INPUT
@@ -13,11 +13,11 @@
 // I2S Microphone Settings
 // Which channel is the I2S microphone on? I2S_CHANNEL_FMT_ONLY_LEFT or I2S_CHANNEL_FMT_ONLY_RIGHT
 // Generally they will default to LEFT - but you may need to attach the L/R pin to GND
-//#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
-#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_33
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_12
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_27
+#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
+//#define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_18
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_5
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_19
 
 // Analog Microphone Settings - ADC1_CHANNEL_7 is GPIO35
 #define ADC_MIC_CHANNEL ADC1_CHANNEL_7
@@ -30,11 +30,11 @@
 // record button
 #define GPIO_BUTTON GPIO_NUM_0
 
-// sdcard
-#define PIN_NUM_MISO GPIO_NUM_19
-#define PIN_NUM_CLK GPIO_NUM_18
-#define PIN_NUM_MOSI GPIO_NUM_23
-#define PIN_NUM_CS GPIO_NUM_5
+// sdcard (unused, as SDIO is fixed to its Pins)
+#define PIN_NUM_MISO GPIO_NUM_2
+#define PIN_NUM_CLK GPIO_NUM_14
+#define PIN_NUM_MOSI GPIO_NUM_15
+#define PIN_NUM_CS GPIO_NUM_14
 
 // i2s config for using the internal ADC
 extern i2s_config_t i2s_adc_config;
